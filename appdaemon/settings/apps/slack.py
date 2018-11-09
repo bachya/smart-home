0cf3d35646b4f40a0bddedc1e1d05bdbe1e21915
Aaron Bach <bachya1208@gmail.com>
2018-11-08 21:15:44 -0700

    Added a /security slash command to Slack

--- appdaemon/settings/apps/slack.py
+++ appdaemon/settings/apps/slack.py
@@ -4,6 +4,11 @@
 import requests
 
 from automation import Base  # type: ignore
+from util import grammatical_list_join  # type: ignore
+
+SECURITY_COMMAND_AWAY = 'away'
+SECURITY_COMMAND_HOME = 'home'
+SECURITY_COMMAND_GOODNIGHT = 'goodnight'
 
 
 class Slack(Base):
@@ -37,18 +42,41 @@ class Slack(Base):
                 'No implementation of slash command handler: {0}'.format(
                     data['command']))
 
-    def thermostat(self, data: dict) -> None:
-        """Alter the thermostat."""
-        target_temp = data['text']
+    def security(self, data: dict) -> None:
+        """Interact with the security manager."""
+        command = data['text']
 
-        if target_temp:
-            self.climate_manager.indoor_temp = int(target_temp)
-            self._respond(
-                "I've set the thermostat to {0}°.".format(target_temp))
-        else:
+        if not command:
+            insecure_entities = self.security_system.get_insecure_entities()
+            if insecure_entities:
+                self._respond(
+                    'These entry points are insecure: {0}.'.format(
+                        grammatical_list_join(insecure_entities)))
+            else:
+                self._respond('The house is locked up and secure.')
+            return
+
+        if command == SECURITY_COMMAND_AWAY:
+            self.call_service('scene/turn_on', entity_id='scene.depart_home')
+            self._respond('The house has been fully secured.')
+        elif command == SECURITY_COMMAND_GOODNIGHT:
+            self.call_service('scene/turn_on', entity_id='scene.good_night')
+            self._respond('The house has been secured for the evening.')
+        elif command == SECURITY_COMMAND_HOME:
+            self.security_system.state = self.security_system.AlarmStates.home
+            self._respond('The security system has been set to "Home".')
+
+    def thermostat(self, data: dict) -> None:
+        """Interact with the thermostat."""
+        command = data['text']
+
+        if not command:
             self._respond(
                 'The thermostat is currently set to {0}° '
-                '(current indoor temperature: {1}°).'.
-                format(
+                '(current indoor temperature: {1}°).'.format(
                     self.climate_manager.indoor_temp,
                     self.climate_manager.average_indoor_temperature))
+            return
+
+        self.climate_manager.indoor_temp = int(command)
+        self._respond("I've set the thermostat to {0}°.".format(command))
