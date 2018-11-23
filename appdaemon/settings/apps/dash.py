2fb66b1aa7311fb0cd0efc8c23e62cf22cfe5ec8
Aaron Bach <bachya1208@gmail.com>
2018-11-23 12:44:41 -0700

    Re-architected Dash actions to be more modular

--- appdaemon/settings/apps/dash.py
+++ appdaemon/settings/apps/dash.py
@@ -1,26 +1,90 @@
 """Define automations for Amazon Dash Buttons."""
-# pylint: disable=unused-argument
+# pylint: disable=too-few-public-methods,unused-argument
 
 from automation import Automation  # type: ignore
 
-OPTION_METHOD_MAP = {
-    'Activate "Good Night"': ('activate_good_night', {}),
-    'Arm security system': ('arm_security_system', {
-        'state': 'home'
-    }),
-    'Bump climate 2°': (
-        'bump_climate', {
-            'amount': 2
-        }),
-    'Toggle Master Bedroom Salt Lamp': (
-        'toggle_salt_lamp', {
-            'entity_id': 'light.salt_lamp_master_bedroom'
-        })
-}
+
+class ButtonAction:
+    """Define a base class for button actions."""
+
+    def __init__(self, hass: Automation, args: dict) -> None:
+        """Initialize."""
+        self._args = args
+        self._hass = hass
+
+    def run(self) -> None:
+        """Run the action."""
+        raise NotImplementedError()
+
+
+class ActivateScene(ButtonAction):
+    """Define an action that turns on a scene."""
+
+    def run(self) -> None:
+        """Turn on the scene."""
+        self._hass.turn_on('scene.{0}'.format(self._args['scene']))
+
+
+class ArmSecuritySystem(ButtonAction):
+    """Define an action that sets the security system in "Home" mode."""
+
+    def run(self) -> None:
+        """Set the security system."""
+        try:
+            state = self._hass.security_manager.States[self._args['state']]
+        except KeyError:
+            self._hass.error('Unknown state: {0}'.format(self._args['state']))
+
+        self._hass.security_manager.state = state
+
+
+class BumpClimate(ButtonAction):
+    """Define an action that bumps the climate 2° in the correct direction."""
+
+    def run(self) -> None:
+        """Bump."""
+        degrees = self._args['degrees']
+        current_mode = self._hass.climate_manager.mode
+
+        if current_mode == self._hass.climate_manager.Modes.cool:
+            degrees *= -1
+
+        self._hass.climate_manager.indoor_temp += degrees
+
+
+class ToggleEntity(ButtonAction):
+    """Toggle an entity."""
+
+    def run(self) -> None:
+        """Toggle."""
+        entity = self._args['entity']
+        domain = entity.split('.')[0]
+
+        self._hass.call_service('{0}/toggle'.format(domain), entity_id=entity)
 
 
 class DashButton(Automation):
     """Define an automation for Amazon Dash Buttons."""
+
+    OBJECT_MAP = {
+        'Activate "Good Night"': (ActivateScene, {
+            'scene': 'good_night'
+        }),
+        'Arm Security System "Home"': (ArmSecuritySystem, {
+            'state': 'home'
+        }),
+        'Bump Climate 2°': (BumpClimate, {
+            'degrees': 2
+        }),
+        'Toggle Christmas Tree': (
+            ToggleEntity, {
+                'entity': 'switch.christmas_tree'
+            }),
+        'Toggle Master Bedroom Salt Lamp': (
+            ToggleEntity, {
+                'entity': 'light.salt_lamp_master_bedroom'
+            }),
+    }
 
     @property
     def action_list(self) -> str:
@@ -41,38 +105,17 @@ class DashButton(Automation):
             'AMAZON_DASH_PRESS',
             button_label=self.properties['friendly_name'])
 
-    def activate_good_night(self) -> None:
-        """Turn on the "Good Night" scene."""
-        self.turn_on('scene.good_night')
-
-    def arm_security_system(self, state: str) -> None:
-        """Set the security system to the specified state."""
-        try:
-            state_enum = self.security_manager.States[state]
-        except KeyError:
-            self.error('Unknown security state: {0}'.format(state))
-
-        self.security_manager.state = state_enum
-
-    def bump_climate(self, amount: int) -> None:
-        """Bump the climate up or down by a certain amount."""
-        if self.climate_manager.mode == self.climate_manager.Modes.cool:
-            amount *= -1
-
-        self.climate_manager.indoor_temp += amount
-
-    def toggle_salt_lamp(self, entity_id: str) -> None:
-        """Toggle the specified salt lamp."""
-        self.call_service('light/toggle', entity_id=entity_id)
-
     def button_pressed(
             self, event_name: str, data: dict, kwargs: dict) -> None:
         """Respond when button is pressed."""
         action_name = self.get_state(self.entities['action_list'])
-        try:
-            method, params = OPTION_METHOD_MAP[action_name]
-        except (AttributeError, KeyError):
+
+        if action_name not in self.OBJECT_MAP:
             self.error('Unknown action: {0}'.format(action_name))
             return
 
-        getattr(self, method)(**params)
+        self.log('Running Dash action: {0}'.format(action_name))
+
+        klass, args = self.OBJECT_MAP[action_name]
+        button = klass(self, args)  # type: ignore
+        button.run()
