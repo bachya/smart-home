d7866ab77747eab6314596a7f5733e4f405a3d90
Aaron Bach <bachya1208@gmail.com>
2019-01-12 12:12:09 -0700

    Removed health-related automations out of climate.py

--- appdaemon/settings/apps/climate.py
+++ appdaemon/settings/apps/climate.py
@@ -2,7 +2,7 @@
 # pylint: disable=attribute-defined-outside-init,unused-argument
 from datetime import timedelta
 from enum import Enum
-from typing import Tuple, Union
+from typing import Tuple
 
 from automation import Automation, Base  # type: ignore
 from util.dt import ceil_dt
@@ -241,47 +241,3 @@ class CycleFan(Automation):
             self.datetime() + timedelta(minutes=15))
 
         self.climate_manager.set_fan_mode(self.climate_manager.FanModes.on)
-
-
-class NotifyBadAqi(Automation):
-    """Define a feature to notify us of bad air quality."""
-
-    @property
-    def current_aqi(self) -> int:
-        """Define a property to get the current AQI."""
-        return int(self.get_state(self.entity_ids['aqi']))
-
-    def initialize(self) -> None:
-        """Initialize."""
-        super().initialize()
-
-        self.notification_sent = False
-
-        self.listen_state(
-            self.bad_aqi_detected,
-            self.entity_ids['hvac_state'],
-            new='cooling',
-            constrain_input_boolean=self.enabled_entity_id)
-
-    def bad_aqi_detected(  # pylint: disable=too-many-arguments
-            self, entity: Union[str, dict], attribute: str, old: str, new: str,
-            kwargs: dict) -> None:
-        """Send select notifications when cooling and poor AQI."""
-        if (not self.notification_sent
-                and self.current_aqi > self.properties['aqi_threshold']):
-            self._log.info('Poor AQI; notifying anyone at home')
-
-            self.notification_manager.send(
-                'AQI is at {0}; consider closing the humidifier vent.'.format(
-                    self.current_aqi),
-                title='Poor AQI 😤',
-                target='home')
-            self.notification_sent = True
-        elif (self.notification_sent
-              and self.current_aqi <= self.properties['aqi_threshold']):
-            self.notification_manager.send(
-                'AQI is at {0}; open the humidifer vent again.'.format(
-                    self.current_aqi),
-                title='Better AQI 😅',
-                target='home')
-            self.notification_sent = True
