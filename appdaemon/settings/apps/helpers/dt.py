bcfe4339dd4669fd7f6ec129b59aa254e8f821af
Aaron Bach <bachya1208@gmail.com>
2019-05-17 21:59:14 -0600

    Updated time_in_between helper

--- appdaemon/settings/apps/helpers/dt.py
+++ appdaemon/settings/apps/helpers/dt.py
@@ -1,5 +1,5 @@
 """Define date/time utilities."""
-from datetime import datetime, timedelta
+from datetime import datetime, time, timedelta
 
 
 def ceil_dt(target_dt: datetime, delta: timedelta) -> datetime:
@@ -22,24 +22,13 @@ def relative_time_of_day(hass) -> str:
     return greeting
 
 
-def time_is_between(
-        hass, target_dt: datetime, start_time: str,
-        end_time: str) -> bool:
-    """Generalization of AppDaemon's now_is_between method."""
-    start_time_dt = hass.parse_time(start_time)  # type: datetime
-    end_time_dt = hass.parse_time(end_time)  # type: datetime
-    start_dt = target_dt.replace(
-        hour=start_time_dt.hour,
-        minute=start_time_dt.minute,
-        second=start_time_dt.second)
-    end_dt = target_dt.replace(
-        hour=end_time_dt.hour,
-        minute=end_time_dt.minute,
-        second=end_time_dt.second)
+def time_is_between(start: time, end: time, target: time = None) -> bool:
+    """Check whether a target/now is between a start and end time."""
+    if target:
+        _target = target
+    else:
+        _target = datetime.now().time()
 
-    if end_dt < start_dt:
-        # Spans midnight
-        if target_dt < start_dt and target_dt < end_dt:
-            target_dt = target_dt + timedelta(days=1)
-        end_dt = end_dt + timedelta(days=1)
-    return start_dt <= target_dt <= end_dt
+    if start < end:
+        return end >= _target >= start
+    return _target >= start or _target <= end
