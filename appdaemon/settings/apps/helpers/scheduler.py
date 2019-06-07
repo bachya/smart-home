51a603f72c7b8fdac4fc8770cc2cf9b6f4168868
Aaron Bach <bachya1208@gmail.com>
2019-06-06 21:13:16 -0600

    Changed "hass" argument to "app" in scheduler

--- appdaemon/settings/apps/helpers/scheduler.py
+++ appdaemon/settings/apps/helpers/scheduler.py
@@ -1,19 +1,26 @@
 """Define scheduling utilities."""
 import datetime
-from typing import Callable, List
+from typing import TYPE_CHECKING, Callable, List
+
+if TYPE_CHECKING:
+    from .core import Base
 
 
 def run_on_days(
-    hass, callback: Callable[..., None], day_list: list, start: datetime.time, **kwargs
+    app: Base,
+    callback: Callable[..., None],
+    day_list: list,
+    start: datetime.time,
+    **kwargs
 ) -> List[str]:
     """Run a callback on certain days (at the specified time)."""
     handle = []
     upcoming_days = []
 
-    today = hass.date()
+    today = app.date()
     todays_event = datetime.datetime.combine(today, start)
 
-    if todays_event > hass.datetime():
+    if todays_event > app.datetime():
         if today.strftime("%A") in day_list:
             upcoming_days.append(today)
 
@@ -25,16 +32,16 @@ def run_on_days(
 
     for day in upcoming_days:
         event = datetime.datetime.combine(day, start)
-        handle.append(hass.run_every(callback, event, 604800, **kwargs))
+        handle.append(app.run_every(callback, event, 604800, **kwargs))
 
     return handle
 
 
 def run_on_weekdays(
-    hass, callback: Callable[..., None], start: datetime.time, **kwargs
+    app: Base, callback: Callable[..., None], start: datetime.time, **kwargs
 ) -> list:
     """Run a callback on weekdays (at the specified time)."""
-    return hass.run_on_days(
+    return app.run_on_days(
         callback,
         ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday"],
         start,
@@ -43,7 +50,7 @@ def run_on_weekdays(
 
 
 def run_on_weekend_days(
-    hass, callback: Callable[..., None], start: datetime.time, **kwargs
+    app: Base, callback: Callable[..., None], start: datetime.time, **kwargs
 ) -> list:
     """Run a callback on weekend days (at the specified time)."""
-    return hass.run_on_days(callback, ["Friday", "Saturday"], start, **kwargs)
+    return app.run_on_days(callback, ["Friday", "Saturday"], start, **kwargs)
