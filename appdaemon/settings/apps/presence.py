e5eb293f4a2b5b773182584d0d4b8381ce0e95db
Aaron Bach <bachya1208@gmail.com>
2019-08-02 12:19:11 -0600

    More bad math

--- appdaemon/settings/apps/presence.py
+++ appdaemon/settings/apps/presence.py
@@ -40,7 +40,6 @@ class PresenceManager(Base):
         self.listen_state(
             self._on_proximity_zone_change,
             self.entity_ids[CONF_PROXIMITY_ZONE_SENSOR],
-            attribute="all",
             duration=60,
         )
 
