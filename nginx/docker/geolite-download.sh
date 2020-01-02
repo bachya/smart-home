00ba968f274bc5b1e74e75646833509a22c11f3c
Aaron Bach <bachya1208@gmail.com>
2020-01-02 10:11:47 -0700

    Updated MaxMindDB retrieval per their compliance with GDPR/CCPA

--- nginx/docker/geolite-download.sh
+++ nginx/docker/geolite-download.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
 # Make a temp dir:
-mkdir -p /tmp/geolite
+mkdir -p /tmp/src
 
 # Download the latest DB:
-wget -N -P /tmp/geolite https://geolite.maxmind.com/download/geoip/database/GeoLite2-Country.tar.gz
-tar xzvf /tmp/geolite/GeoLite2-Country.tar.gz -C /tmp/geolite --strip-components=1
-mv /tmp/geolite/GeoLite2-Country.mmdb /usr/local/share/GeoIP/ \
+wget -O /tmp/src/GeoLite2-Country.tar.gz "https://download.maxmind.com/app/geoip_download?edition_id=GeoLite2-Country&license_key=fCICsoZwXeb3gTO9&suffix=tar.gz"
+tar xzvf /tmp/src/GeoLite2-Country.tar.gz -C /tmp/src --strip-components=1
+mv /tmp/src/GeoLite2-Country.mmdb /usr/local/share/GeoIP
 
 # Delete the temp dir:
-rm -rf /tmp/geolite
+rm -rf /tmp/src
 
 # Log:
 echo "$(date +%Y-%m-%d\ %H:%m\ %p): Latest GeoLite2 database downloaded" >> /var/log/geolite-download.log
