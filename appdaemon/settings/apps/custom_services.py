dea22e6cc739886b0ed051d94c62757cea2750b3
Aaron Bach <bachya1208@gmail.com>
2020-02-14 21:32:30 -0700

    Fix a variety of issues with custom service registration

--- appdaemon/settings/apps/custom_services.py
+++ appdaemon/settings/apps/custom_services.py
@@ -1,8 +1,6 @@
 """Define custom services."""
-from typing import List
-
-import helpers.config_validation as cv
 import voluptuous as vol
+
 from const import (
     CONF_NOTIFICATION_DATA,
     CONF_NOTIFICATION_INTERVAL,
@@ -13,50 +11,39 @@ from const import (
     CONF_NOTIFICATION_WHEN,
 )
 from core import APP_SCHEMA, Base
+from helpers import config_validation as cv
 from notification import send_notification
+
+CONF_SERVICE_NAME = "service_name"
 
 
 class SendNotification(Base):  # pylint: disable=too-few-public-methods
     """Define an automation that sends notifications on events."""
 
-    APP_SCHEMA = APP_SCHEMA.extend(
-        {
-            vol.Required(CONF_NOTIFICATION_TARGET): vol.Any(
-                cv.notification_target, List[cv.notification_target]
-            ),
-            vol.Required(CONF_NOTIFICATION_MESSAGE): cv.string,
-            vol.Optional(CONF_NOTIFICATION_TITLE): cv.string,
-            vol.Optional(CONF_NOTIFICATION_WHEN): cv.string,
-            vol.Optional(CONF_NOTIFICATION_INTERVAL): cv.positive_int,
-            vol.Optional(CONF_NOTIFICATION_ITERATIONS): cv.positive_int,
-            vol.Optional(CONF_NOTIFICATION_DATA): dict,
-        }
-    )
-
-    SERVICE = "appdaemon/send_notification"
+    APP_SCHEMA = APP_SCHEMA.extend({vol.Required(CONF_SERVICE_NAME): cv.string})
 
     def configure(self) -> None:
         """Configure."""
-        # We'll use this class multiple times, but only want to register the service
-        # one time:
-        if self.SERVICE not in self.list_services():
-            self.register_service(self.SERVICE, self._send_notification)
+        self.register_service(
+            f"appdaemon/{self.args[CONF_SERVICE_NAME]}", self._service_called
+        )
 
-    def _send_notification(self) -> None:
+    def _service_called(
+        self, namespace: str, domain: str, service: str, data: dict
+    ) -> None:
         """Send a notification."""
         kwargs = {
-            CONF_NOTIFICATION_DATA: self.args.get(CONF_NOTIFICATION_DATA),
-            CONF_NOTIFICATION_INTERVAL: self.args.get(CONF_NOTIFICATION_INTERVAL),
-            CONF_NOTIFICATION_ITERATIONS: self.args.get(CONF_NOTIFICATION_ITERATIONS),
-            CONF_NOTIFICATION_TITLE: self.args.get(CONF_NOTIFICATION_TITLE),
+            "data": data.get(CONF_NOTIFICATION_DATA),
+            "interval": data.get(CONF_NOTIFICATION_INTERVAL),
+            "iterations": data.get(CONF_NOTIFICATION_ITERATIONS),
+            "title": data.get(CONF_NOTIFICATION_TITLE),
         }
-        if CONF_NOTIFICATION_WHEN in self.args:
-            kwargs[CONF_NOTIFICATION_WHEN] = self.parse_datetime(
-                self.args[CONF_NOTIFICATION_WHEN]
-            )
+        if "when" in data:
+            kwargs["when"] = self.parse_datetime(data[CONF_NOTIFICATION_WHEN])
 
         send_notification(
-            self.args[CONF_NOTIFICATION_TARGET],
-            self.args[CONF_NOTIFICATION_MESSAGE],
+            self,
+            data[CONF_NOTIFICATION_TARGET],
+            data[CONF_NOTIFICATION_MESSAGE],
             **kwargs,
         )
