e5aa3e57ccf3cbb75a6791aaa7236fe7b7d69b9a
Aaron Bach <bachya1208@gmail.com>
2020-03-17 20:35:37 -0600

    Remove dependencies for forwarder serverless function

--- serverless/mothership/alexa_forwarder.py
+++ serverless/mothership/alexa_forwarder.py
@@ -1,13 +1,43 @@
 """Define serverless functions to work with AppDaemon."""
-import requests
+# -*- coding: utf-8 -*-
+import os
+import json
+import logging
+import urllib3
 
-APPDAEMON_API_PASSWORD = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
-APPDAEMON_URL = "https://appdaemon.myserver.com/api/appdaemon/alexa"
+_DEBUG = bool(os.environ.get("DEBUG"))
+
+_LOGGER = logging.getLogger("AppDaemon-Intents")
+_LOGGER.setLevel(logging.DEBUG if _DEBUG else logging.INFO)
 
 
 def forward_invocation_handler(event, context):  # pylint: disable=unused-argument
-    """Forward an Alexa intent invocation to AppDaemon."""
-    resp = requests.post(
-        APPDAEMON_URL, params={"api_password": APPDAEMON_API_PASSWORD}, json=event
+    """Handle incoming Alexa directive."""
+    _LOGGER.debug("Event: %s", event)
+
+    base_url = os.environ.get("APPDAEMON_BASE_URL")
+    assert base_url is not None, "Please set APPDAEMON_BASE_URL environment variable"
+
+    http = urllib3.PoolManager(
+        cert_reqs="CERT_REQUIRED", timeout=urllib3.Timeout(connect=2.0, read=10.0)
     )
-    return resp.json()
+
+    response = http.request(
+        "POST",
+        "{}/api/appdaemon/alexa?api_password={}".format(
+            base_url, os.environ.get("APPDAEMON_API_PASSWORD")
+        ),
+        body=json.dumps(event).encode("utf-8"),
+    )
+    if response.status >= 400:
+        return {
+            "event": {
+                "payload": {
+                    "type": "INVALID_AUTHORIZATION_CREDENTIAL"
+                    if response.status in (401, 403)
+                    else "INTERNAL_ERROR",
+                    "message": response.data.decode("utf-8"),
+                }
+            }
+        }
+    return json.loads(response.data.decode("utf-8"))
