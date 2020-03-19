6390764e67607f2a134be753547a8965778e812c
Aaron Bach <bachya1208@gmail.com>
2020-03-19 08:58:35 -0600

    Wrap Python script in a function

--- home-assistant/settings/python_scripts/log.py
+++ home-assistant/settings/python_scripts/log.py
@@ -1,10 +1,17 @@
 """Define a Python script to insert log entries into the HASS log."""
-LOG_LEVEL = data["level"]
-MESSAGE = data["MESSAGE"]
 
-if LOG_LEVEL == "ERROR":
-    logger.error(MESSAGE)
-elif LOG_LEVEL == "WARNING":
-    logger.warning(MESSAGE)
-else:
-    logger.info(MESSAGE)
+
+def main():
+    """Run the script."""
+    log_level = data["level"]
+    message = data["message"]
+
+    if log_level == "ERROR":
+        logger.error(message)
+    elif log_level == "WARNING":
+        logger.warning(message)
+    else:
+        logger.info(message)
+
+
+main()
