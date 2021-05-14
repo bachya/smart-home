8e3c08da969fcd313ab0689eb76f60ee597fe610
Aaron Bach <bachya1208@gmail.com>
2021-05-14 08:22:13 -0600

    Slipstream IQVIA bugfix

---
+++ hass/settings/custom_components/iqvia/__init__.py
@@ -0,0 +1,169 @@
+"""Support for IQVIA."""
+import asyncio
+from datetime import timedelta
+from functools import partial
+
+from pyiqvia import Client
+from pyiqvia.errors import IQVIAError
+
+from homeassistant.components.sensor import SensorEntity
+from homeassistant.const import ATTR_ATTRIBUTION
+from homeassistant.core import callback
+from homeassistant.exceptions import ConfigEntryNotReady
+from homeassistant.helpers import aiohttp_client
+from homeassistant.helpers.update_coordinator import (
+    CoordinatorEntity,
+    DataUpdateCoordinator,
+    UpdateFailed,
+)
+
+from .const import (
+    CONF_ZIP_CODE,
+    DATA_COORDINATOR,
+    DOMAIN,
+    LOGGER,
+    TYPE_ALLERGY_FORECAST,
+    TYPE_ALLERGY_INDEX,
+    TYPE_ALLERGY_OUTLOOK,
+    TYPE_ASTHMA_FORECAST,
+    TYPE_ASTHMA_INDEX,
+    TYPE_DISEASE_FORECAST,
+    TYPE_DISEASE_INDEX,
+)
+
+DEFAULT_ATTRIBUTION = "Data provided by IQVIA™"
+DEFAULT_SCAN_INTERVAL = timedelta(minutes=30)
+
+PLATFORMS = ["sensor"]
+
+
+async def async_setup_entry(hass, entry):
+    """Set up IQVIA as config entry."""
+    hass.data.setdefault(DOMAIN, {})
+    coordinators = {}
+
+    if not entry.unique_id:
+        # If the config entry doesn't already have a unique ID, set one:
+        hass.config_entries.async_update_entry(
+            entry, **{"unique_id": entry.data[CONF_ZIP_CODE]}
+        )
+
+    websession = aiohttp_client.async_get_clientsession(hass)
+    client = Client(entry.data[CONF_ZIP_CODE], session=websession)
+
+    async def async_get_data_from_api(api_coro):
+        """Get data from a particular API coroutine."""
+        try:
+            return await api_coro()
+        except IQVIAError as err:
+            raise UpdateFailed(err)
+
+    init_data_update_tasks = []
+    for sensor_type, api_coro in [
+        (TYPE_ALLERGY_FORECAST, client.allergens.extended),
+        (TYPE_ALLERGY_INDEX, client.allergens.current),
+        (TYPE_ALLERGY_OUTLOOK, client.allergens.outlook),
+        (TYPE_ASTHMA_FORECAST, client.asthma.extended),
+        (TYPE_ASTHMA_INDEX, client.asthma.current),
+        (TYPE_DISEASE_FORECAST, client.disease.extended),
+        (TYPE_DISEASE_INDEX, client.disease.current),
+    ]:
+        coordinator = coordinators[sensor_type] = DataUpdateCoordinator(
+            hass,
+            LOGGER,
+            name=f"{entry.data[CONF_ZIP_CODE]} {sensor_type}",
+            update_interval=DEFAULT_SCAN_INTERVAL,
+            update_method=partial(async_get_data_from_api, api_coro),
+        )
+        init_data_update_tasks.append(coordinator.async_refresh())
+
+    results = await asyncio.gather(*init_data_update_tasks, return_exceptions=True)
+    if all(isinstance(result, Exception) for result in results):
+        # The IQVIA API can be selectively flaky, meaning that any number of the setup
+        # API calls could fail. We only retry integration setup if *all* of the initial
+        # API calls fail:
+        raise ConfigEntryNotReady()
+
+    hass.data[DOMAIN].setdefault(DATA_COORDINATOR, {})[entry.entry_id] = coordinators
+    hass.config_entries.async_setup_platforms(entry, PLATFORMS)
+
+    return True
+
+
+async def async_unload_entry(hass, entry):
+    """Unload an OpenUV config entry."""
+    unload_ok = await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
+    if unload_ok:
+        hass.data[DOMAIN][DATA_COORDINATOR].pop(entry.entry_id)
+    return unload_ok
+
+
+class IQVIAEntity(CoordinatorEntity, SensorEntity):
+    """Define a base IQVIA entity."""
+
+    def __init__(self, coordinator, entry, sensor_type, name, icon):
+        """Initialize."""
+        super().__init__(coordinator)
+        self._attrs = {ATTR_ATTRIBUTION: DEFAULT_ATTRIBUTION}
+        self._entry = entry
+        self._icon = icon
+        self._name = name
+        self._state = None
+        self._type = sensor_type
+
+    @property
+    def extra_state_attributes(self):
+        """Return the device state attributes."""
+        return self._attrs
+
+    @property
+    def icon(self):
+        """Return the icon."""
+        return self._icon
+
+    @property
+    def name(self):
+        """Return the name."""
+        return self._name
+
+    @property
+    def state(self):
+        """Return the state."""
+        return self._state
+
+    @property
+    def unique_id(self):
+        """Return a unique, Home Assistant friendly identifier for this entity."""
+        return f"{self._entry.data[CONF_ZIP_CODE]}_{self._type}"
+
+    @property
+    def unit_of_measurement(self):
+        """Return the unit the value is expressed in."""
+        return "index"
+
+    @callback
+    def _handle_coordinator_update(self) -> None:
+        """Handle updated data from the coordinator."""
+        if not self.coordinator.last_update_success:
+            return
+
+        self.update_from_latest_data()
+        self.async_write_ha_state()
+
+    async def async_added_to_hass(self):
+        """Register callbacks."""
+        await super().async_added_to_hass()
+
+        if self._type == TYPE_ALLERGY_FORECAST:
+            self.async_on_remove(
+                self.hass.data[DOMAIN][DATA_COORDINATOR][self._entry.entry_id][
+                    TYPE_ALLERGY_OUTLOOK
+                ].async_add_listener(self._handle_coordinator_update)
+            )
+
+        self.update_from_latest_data()
+
+    @callback
+    def update_from_latest_data(self):
+        """Update the entity from the latest data."""
+        raise NotImplementedError
---
+++ hass/settings/custom_components/iqvia/sensor.py
@@ -0,0 +1,207 @@
+"""Support for IQVIA sensors."""
+from statistics import mean
+
+import numpy as np
+
+from homeassistant.const import ATTR_STATE
+from homeassistant.core import callback
+
+from . import IQVIAEntity
+from .const import (
+    DATA_COORDINATOR,
+    DOMAIN,
+    SENSORS,
+    TYPE_ALLERGY_FORECAST,
+    TYPE_ALLERGY_INDEX,
+    TYPE_ALLERGY_OUTLOOK,
+    TYPE_ALLERGY_TODAY,
+    TYPE_ALLERGY_TOMORROW,
+    TYPE_ASTHMA_FORECAST,
+    TYPE_ASTHMA_INDEX,
+    TYPE_ASTHMA_TODAY,
+    TYPE_ASTHMA_TOMORROW,
+    TYPE_DISEASE_FORECAST,
+    TYPE_DISEASE_INDEX,
+    TYPE_DISEASE_TODAY,
+)
+
+ATTR_ALLERGEN_AMOUNT = "allergen_amount"
+ATTR_ALLERGEN_GENUS = "allergen_genus"
+ATTR_ALLERGEN_NAME = "allergen_name"
+ATTR_ALLERGEN_TYPE = "allergen_type"
+ATTR_CITY = "city"
+ATTR_OUTLOOK = "outlook"
+ATTR_RATING = "rating"
+ATTR_SEASON = "season"
+ATTR_TREND = "trend"
+ATTR_ZIP_CODE = "zip_code"
+
+API_CATEGORY_MAPPING = {
+    TYPE_ALLERGY_TODAY: TYPE_ALLERGY_INDEX,
+    TYPE_ALLERGY_TOMORROW: TYPE_ALLERGY_INDEX,
+    TYPE_ALLERGY_TOMORROW: TYPE_ALLERGY_INDEX,
+    TYPE_ASTHMA_TODAY: TYPE_ASTHMA_INDEX,
+    TYPE_ASTHMA_TOMORROW: TYPE_ASTHMA_INDEX,
+    TYPE_DISEASE_TODAY: TYPE_DISEASE_INDEX,
+}
+
+RATING_MAPPING = [
+    {"label": "Low", "minimum": 0.0, "maximum": 2.4},
+    {"label": "Low/Medium", "minimum": 2.5, "maximum": 4.8},
+    {"label": "Medium", "minimum": 4.9, "maximum": 7.2},
+    {"label": "Medium/High", "minimum": 7.3, "maximum": 9.6},
+    {"label": "High", "minimum": 9.7, "maximum": 12},
+]
+
+TREND_FLAT = "Flat"
+TREND_INCREASING = "Increasing"
+TREND_SUBSIDING = "Subsiding"
+
+
+async def async_setup_entry(hass, entry, async_add_entities):
+    """Set up IQVIA sensors based on a config entry."""
+    sensor_class_mapping = {
+        TYPE_ALLERGY_FORECAST: ForecastSensor,
+        TYPE_ALLERGY_TODAY: IndexSensor,
+        TYPE_ALLERGY_TOMORROW: IndexSensor,
+        TYPE_ASTHMA_FORECAST: ForecastSensor,
+        TYPE_ASTHMA_TODAY: IndexSensor,
+        TYPE_ASTHMA_TOMORROW: IndexSensor,
+        TYPE_DISEASE_FORECAST: ForecastSensor,
+        TYPE_DISEASE_TODAY: IndexSensor,
+    }
+
+    sensors = []
+    for sensor_type, (name, icon) in SENSORS.items():
+        api_category = API_CATEGORY_MAPPING.get(sensor_type, sensor_type)
+        coordinator = hass.data[DOMAIN][DATA_COORDINATOR][entry.entry_id][api_category]
+        sensor_class = sensor_class_mapping[sensor_type]
+
+        sensors.append(sensor_class(coordinator, entry, sensor_type, name, icon))
+
+    async_add_entities(sensors)
+
+
+def calculate_trend(indices):
+    """Calculate the "moving average" of a set of indices."""
+    index_range = np.arange(0, len(indices))
+    index_array = np.array(indices)
+    linear_fit = np.polyfit(index_range, index_array, 1)
+    slope = round(linear_fit[0], 2)
+
+    if slope > 0:
+        return TREND_INCREASING
+
+    if slope < 0:
+        return TREND_SUBSIDING
+
+    return TREND_FLAT
+
+
+class ForecastSensor(IQVIAEntity):
+    """Define sensor related to forecast data."""
+
+    @callback
+    def update_from_latest_data(self):
+        """Update the sensor."""
+        if not self.coordinator.data:
+            return
+
+        data = self.coordinator.data.get("Location", {})
+
+        if not data.get("periods"):
+            return
+
+        indices = [p["Index"] for p in data["periods"]]
+        average = round(mean(indices), 1)
+        [rating] = [
+            i["label"]
+            for i in RATING_MAPPING
+            if i["minimum"] <= average <= i["maximum"]
+        ]
+
+        self._attrs.update(
+            {
+                ATTR_CITY: data["City"].title(),
+                ATTR_RATING: rating,
+                ATTR_STATE: data["State"],
+                ATTR_TREND: calculate_trend(indices),
+                ATTR_ZIP_CODE: data["ZIP"],
+            }
+        )
+
+        if self._type == TYPE_ALLERGY_FORECAST:
+            outlook_coordinator = self.hass.data[DOMAIN][DATA_COORDINATOR][
+                self._entry.entry_id
+            ][TYPE_ALLERGY_OUTLOOK]
+            self._attrs[ATTR_OUTLOOK] = outlook_coordinator.data.get("Outlook")
+            self._attrs[ATTR_SEASON] = outlook_coordinator.data.get("Season")
+
+        self._state = average
+
+
+class IndexSensor(IQVIAEntity):
+    """Define sensor related to indices."""
+
+    @callback
+    def update_from_latest_data(self):
+        """Update the sensor."""
+        if not self.coordinator.last_update_success:
+            return
+
+        try:
+            if self._type in (TYPE_ALLERGY_TODAY, TYPE_ALLERGY_TOMORROW):
+                data = self.coordinator.data.get("Location")
+            elif self._type in (TYPE_ASTHMA_TODAY, TYPE_ASTHMA_TOMORROW):
+                data = self.coordinator.data.get("Location")
+            elif self._type == TYPE_DISEASE_TODAY:
+                data = self.coordinator.data.get("Location")
+        except KeyError:
+            return
+
+        key = self._type.split("_")[-1].title()
+
+        try:
+            [period] = [p for p in data["periods"] if p["Type"] == key]
+        except ValueError:
+            return
+
+        [rating] = [
+            i["label"]
+            for i in RATING_MAPPING
+            if i["minimum"] <= period["Index"] <= i["maximum"]
+        ]
+
+        self._attrs.update(
+            {
+                ATTR_CITY: data["City"].title(),
+                ATTR_RATING: rating,
+                ATTR_STATE: data["State"],
+                ATTR_ZIP_CODE: data["ZIP"],
+            }
+        )
+
+        if self._type in (TYPE_ALLERGY_TODAY, TYPE_ALLERGY_TOMORROW):
+            for idx, attrs in enumerate(period["Triggers"]):
+                index = idx + 1
+                self._attrs.update(
+                    {
+                        f"{ATTR_ALLERGEN_GENUS}_{index}": attrs["Genus"],
+                        f"{ATTR_ALLERGEN_NAME}_{index}": attrs["Name"],
+                        f"{ATTR_ALLERGEN_TYPE}_{index}": attrs["PlantType"],
+                    }
+                )
+        elif self._type in (TYPE_ASTHMA_TODAY, TYPE_ASTHMA_TOMORROW):
+            for idx, attrs in enumerate(period["Triggers"]):
+                index = idx + 1
+                self._attrs.update(
+                    {
+                        f"{ATTR_ALLERGEN_NAME}_{index}": attrs["Name"],
+                        f"{ATTR_ALLERGEN_AMOUNT}_{index}": attrs["PPM"],
+                    }
+                )
+        elif self._type == TYPE_DISEASE_TODAY:
+            for attrs in period["Triggers"]:
+                self._attrs[f"{attrs['Name'].lower()}_index"] = attrs["Index"]
+
+        self._state = period["Index"]
